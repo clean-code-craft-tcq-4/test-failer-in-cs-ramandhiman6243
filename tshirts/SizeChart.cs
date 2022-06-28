37271078aa53449e8bb72db92976ef703343cb1b
Ramandeep Singh (MS/ECI-XC) <Ramandeep.Singh2@in.bosch.com>
2022-06-28 23:28:44 +0530

    Fixed tshirt.cs

---
+++ tshirts/SizeChart.cs
@@ -0,0 +1,21 @@
+﻿namespace TshirtSpace
+{
+    public class SizeChart
+    {
+        public static string GetSize(int cms)
+        {
+            if (cms <= 38)
+            {
+                return "S";
+            }
+            else if (cms > 38 && cms < 42)
+            {
+                return "M";
+            }
+            else
+            {
+                return "L";
+            }
+        }
+    }
+}
